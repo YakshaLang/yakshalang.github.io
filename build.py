0f662114c1ad56c56546eaaab84a87b8c1421437
tashvit <53877214+tashvit@users.noreply.github.com>
2022-10-04 19:47:55 +0100

    update home page
    New home page structure, CSS, JavaScript and logo

--- build.py
+++ build.py
@@ -7,8 +7,6 @@ from docbox import docbox
 def main():
     path = os.path.abspath(".")
     subprocess.run("css-minify -d ./css/ -o ./docs/assets", shell=True)
-    docbox.conv(["--input", "yaksha_home", "-o", "docs/index.html", "--title",
-                 "Yaksha Programming Language", "--no-number"], root=path)
     docbox.conv(["--all-headers-in-toc", "--input", "yaksha_lib_docs", "-o", "docs/library-docs.html", "--title",
                  "Yaksha Programming Language"], root=path)
     docbox.conv(["--all-headers-in-toc", "--input", "yaksha_docs", "-o", "docs/documentation.html", "--title",
