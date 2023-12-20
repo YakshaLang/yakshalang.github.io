0d9b9b0ed8317750fc0c08aa5debe51f6bab02e0
GDWR <gregory.dwr@gmail.com>
2023-12-20 21:54:56 +0000

    Add lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-
 import yakshaGrammar from './yaksha.tmLanguage.json';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gdwr.github.io',
   base: '/yakshalang.github.io',
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   markdown: {
     shikiConfig: {
       langs: [{
