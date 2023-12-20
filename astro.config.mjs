a9063e3630601f853d7979998aed2d1b7ed609ca
GDWR <gregory.dwr@gmail.com>
2023-12-20 18:49:11 +0000

    Add yaksha code highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,20 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import yakshaGrammar from './yaksha.tmLanguage.json';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gdwr.github.io',
   base: '/yakshalang.github.io',
   integrations: [tailwind()],
+  markdown: {
+    shikiConfig: {
+      langs: [{
+        name: 'yaksha',
+        scopeName: 'source.yaksha',
+        ...yakshaGrammar
+      }, "c", "scheme"]
+    }
+  }
 });
