c5198c0ab4f00197ca6031399d760fe81252774c
GDWR <gregory.dwr@gmail.com>
2023-12-21 10:46:43 +0000

    Use grammar file from main repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import yakshaGrammar from './yaksha.tmLanguage.json';
 
 import mdx from "@astrojs/mdx";
+import fetch from 'node-fetch';
+
+// Downloads yaksha grammar from main repo, may change in the future?
+const yakshaGrammarUrl = "https://raw.githubusercontent.com/YakshaLang/Yaksha/main/editor/vscode/syntaxes/yaksha.json";
+var resp = await fetch(yakshaGrammarUrl, {method: "GET"});
+let yakshaGrammar = await resp.json();
+
 
 // https://astro.build/config
 export default defineConfig({
