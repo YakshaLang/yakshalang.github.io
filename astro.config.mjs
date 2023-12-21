70051bdf31fc8b164e6ace8504b1d46415c88903
GDWR <gregory.dwr@gmail.com>
2023-12-21 14:07:21 +0000

    Add python and bash to syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
         name: 'yaksha',
         scopeName: 'source.yaksha',
         ...yakshaGrammar
-      }, "c", "scheme"]
+      }, "c", "bash", "scheme", "python"]
     }
   }
 });
